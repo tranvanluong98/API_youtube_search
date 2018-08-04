d5112b718d67845a5830d3753fe5c3c6e5195ab5
tranvanluong98 <luongbeat98@gmail.com>
2018-08-04 16:14:14 +0700

    youtubesearch

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var pageToken = "";
+var isLoading = false;
 $(document).ready(function () {
     $(document).keypress(function (e) {
         if (e.keyCode == 13) {
@@ -5,6 +7,7 @@ $(document).ready(function () {
             var str = $('#keyword').val()
             // clear 
             $('#result-list').html('');
+            // $('.lds-hourglass').css("opacity","1");
             $.ajax({
                 url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + str + "&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw",
                 type: "GET",
@@ -17,18 +20,19 @@ $(document).ready(function () {
                         var videoUrlImg = contentSearch.items[i].snippet.thumbnails.default.url
                         var videoTitle = contentSearch.items[i].snippet.title
                         var videoDescription = contentSearch.items[i].snippet.description
-                        $("#result-list").append(`<a class="result col-md-12" href="https://youtube.com/watch?v=${videoId}&autoplay=true" target="_black">
-                                            <img src="` + videoUrlImg + `" alt="">
-                            <div class="video_info">
-                                <h2 class="title">` + videoTitle + ` </h2>
-                                <p class="description">` + videoDescription + ` </p>
-                                <span class="views">Click here to Watch Video</span>
-                            </div>
-                    </a>`);
+                        if (contentSearch.items[i].id.kind == "youtube#video") {
+                            $("#result-list").append(`<a class="result col-md-12" href="https://youtube.com/watch?v=${videoId}&autoplay=true" target="_black">
+                        <img src="` + videoUrlImg + `" alt="">
+        <div class="video_info">
+            <h2 class="title">` + videoTitle + ` </h2>
+            <p class="description">` + videoDescription + ` </p>
+            <span class="views">Click here to Watch Video</span>
+        </div>
+</a>`);
+                        }
 
                     }
 
-                    contentSearch.hide()
 
                 },
 
@@ -40,12 +44,13 @@ $(document).ready(function () {
 
     });
 
-    $('#search').on("submit", (event) => {
-        event.preventDefault();
+    $('#keyword').on("input", () => {
+      
         var str = $('#keyword').val()
         // clear 
+       
         $('#result-list').html('');
-
+        // $('.lds-hourglass').css("opacity","1");
         $.ajax({
             url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + str + "&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw",
             type: "GET",
@@ -77,11 +82,81 @@ $(document).ready(function () {
             }
         });
 
-
-
     })
 
 
-
-
 })
+
+$(window).on("scroll", () => {
+    if ($(document).height() - ($(window).height() + $(window).scrollTop()) <= 400) {
+        console.log("Loading continue")
+        
+           
+        var str = $('#keyword').val()
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + str + "&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw",
+            type: "GET",
+
+            success: function (contentSearch) {
+                pageToken = contentSearch.nextPageToken ? contentSearch.nextPageToken : null;
+                for (let i = 0; i < contentSearch.items.length; i++) {
+                    var videoId = contentSearch.items[i].id.videoId;
+                    var videoUrlImg = contentSearch.items[i].snippet.thumbnails.default.url
+                    var videoTitle = contentSearch.items[i].snippet.title
+                    var videoDescription = contentSearch.items[i].snippet.description
+
+
+                    $("#result-list").append(`<a class="result col-md-12" href="https://youtube.com/watch?v=${videoId}&autoplay=true&pageToken=${pageToken}" target="_black">
+                         <img src="` + videoUrlImg + `" alt="">
+                        <div class="video_info">
+                            <h2 class="title">` + videoTitle + ` </h2>
+                            <p class="description">` + videoDescription + ` </p>
+                            <span class="views">Click here to Watch Video</span>
+                        </div>
+                </a>`);
+
+                }
+               
+                
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+        
+    }
+})
+
+function search(keyword){
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + str + "&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw",
+        type: "GET",
+
+        success: function (contentSearch) {
+            pageToken = contentSearch.nextPageToken ? contentSearch.nextPageToken : null;
+            for (let i = 0; i < contentSearch.items.length; i++) {
+                var videoId = contentSearch.items[i].id.videoId;
+                var videoUrlImg = contentSearch.items[i].snippet.thumbnails.default.url
+                var videoTitle = contentSearch.items[i].snippet.title
+                var videoDescription = contentSearch.items[i].snippet.description
+
+
+                $("#result-list").append(`<a class="result col-md-12" href="https://youtube.com/watch?v=${videoId}&autoplay=true&pageToken=${pageToken}" target="_black">
+                     <img src="` + videoUrlImg + `" alt="">
+                    <div class="video_info">
+                        <h2 class="title">` + videoTitle + ` </h2>
+                        <p class="description">` + videoDescription + ` </p>
+                        <span class="views">Click here to Watch Video</span>
+                    </div>
+            </a>`);
+
+            }
+           
+            
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    
+}
